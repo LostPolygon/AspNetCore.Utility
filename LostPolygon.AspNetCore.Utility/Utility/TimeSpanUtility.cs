8f86feb72bed1aba446b84a5e468093441a03f4c
Serhii Yolkin <serhii.yolkin@gmail.com>
2020-02-10 14:43:11 +0100

    Updated deps Time utilities tweaks

--- LostPolygon.AspNetCore.Utility/Utility/TimeSpanUtility.cs
+++ LostPolygon.AspNetCore.Utility/Utility/TimeSpanUtility.cs
@@ -3,7 +3,7 @@ using System;
 namespace LostPolygon.AspNetCore.Utility {
     public static class TimeSpanUtility {
         public static string FormatTimeZoneOffset(TimeSpan timeSpan) {
-            return String.Format("{0}{1:00}:{2:00}", timeSpan.Hours >= 0 ? "+" : "−", timeSpan.Hours, timeSpan.Minutes);
+            return String.Format("{0}{1:00}:{2:00}", timeSpan.Hours >= 0 ? "+" : "−", Math.Abs(timeSpan.Hours), timeSpan.Minutes);
         }
     }
 }
--- LostPolygon.AspNetCore.Utility/Utility/TimeZoneUtility.cs
+++ LostPolygon.AspNetCore.Utility/Utility/TimeZoneUtility.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Runtime.InteropServices;
 using TimeZoneConverter;
 
@@ -6,27 +5,13 @@ namespace LostPolygon.AspNetCore.Utility {
     public static class TimeZoneUtility {
         private static readonly bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
-        public static string TimeZoneWindowsToIana(string windowsTimeZoneId) {
+        public static string TimeZoneIdToIana(string timeZoneId) {
             // Windows uses its own timezone format so we need to convert between them,
             // other OS's only uses IANA, so use the data as-is for those
             if (IsWindows)
-                return TZConvert.WindowsToIana(windowsTimeZoneId, "001");
+                return TZConvert.WindowsToIana(timeZoneId, "001");
 
-            return windowsTimeZoneId;
-        }
-
-        public static string GetDisplayNameSafe(this TimeZoneInfo timeZoneInfo) {
-            string name = timeZoneInfo.DisplayName;
-            if (!String.IsNullOrWhiteSpace(name))
-                return name;
-
-            name = timeZoneInfo.StandardName;
-            if (String.IsNullOrWhiteSpace(name)) {
-                name = timeZoneInfo.Id;
-            }
-
-            name = $"(UTC{TimeSpanUtility.FormatTimeZoneOffset(timeZoneInfo.BaseUtcOffset)}) {name}";
-            return name;
+            return timeZoneId;
         }
     }
 }
