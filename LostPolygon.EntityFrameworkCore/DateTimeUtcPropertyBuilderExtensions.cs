1555d3e1fdb2f8d7014c725687bd9db8d5e2102d
Serhii Yolkin <serhii.yolkin@gmail.com>
2020-09-24 11:58:45 +0200

    Added DateTimeUtcPropertyBuilderExtensions to allow enforcing DateTime properties of DB entities to have UTC kind

---
+++ LostPolygon.EntityFrameworkCore/DateTimeUtcPropertyBuilderExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LostPolygon.EntityFrameworkCore {
+    public static class DateTimeUtcPropertyBuilderExtensions {
+        private static DateTime FromCodeToData(DateTime fromCode, string name)
+            => fromCode.Kind == DateTimeKind.Utc ? fromCode : throw new InvalidOperationException($"Column {name} only accepts UTC date-time values");
+
+        private static DateTime FromDataToCode(DateTime fromData)
+            => fromData.Kind == DateTimeKind.Unspecified ? DateTime.SpecifyKind(fromData, DateTimeKind.Utc) : fromData.ToUniversalTime();
+
+        public static PropertyBuilder<DateTime?> UsesUtc(this PropertyBuilder<DateTime?> property) {
+            return property;
+            string? name = property.Metadata.Name;
+            return property.HasConversion<DateTime?>(
+                fromCode => fromCode != null ? FromCodeToData(fromCode.Value, name) : default,
+                fromData => fromData != null ? FromDataToCode(fromData.Value) : default
+            );
+        }
+
+        public static PropertyBuilder<DateTime> UsesUtc(this PropertyBuilder<DateTime> property) {
+            return property;
+            string? name = property.Metadata.Name;
+            return property.HasConversion(fromCode => FromCodeToData(fromCode, name), fromData => FromDataToCode(fromData));
+        }
+    }
+}
