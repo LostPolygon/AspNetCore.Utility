e4b55206ef0db737ad2b2351f95f31314d5494f8
Serhii Yolkin <serhii.yolkin@gmail.com>
2021-10-10 15:29:49 +0200

    feat: added PrefixLoggerWrapper.cs

---
+++ LostPolygon.AspNetCore.Utility/Utility/PrefixLoggerWrapper.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace LostPolygon.AspNetCore.Utility {
+    public class TaggedLoggerWrapper : ILogger {
+        private readonly ILogger _loggerImplementation;
+        private readonly string _prefix;
+
+        public TaggedLoggerWrapper(ILogger loggerImplementation, string prefix) {
+            _loggerImplementation = loggerImplementation;
+            _prefix = prefix;
+        }
+
+        public IDisposable BeginScope<TState>(TState state) {
+            return _loggerImplementation.BeginScope(state);
+        }
+
+        public bool IsEnabled(LogLevel logLevel) {
+            return _loggerImplementation.IsEnabled(logLevel);
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) {
+            string PrefixFormatter(TState sourceState, Exception sourceException) => _prefix + formatter(sourceState, sourceException);
+
+            _loggerImplementation.Log(logLevel, eventId, state, exception, PrefixFormatter);
+        }
+    }
+}
