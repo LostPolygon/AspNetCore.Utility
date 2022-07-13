d1af5d0a4769b169a8c1c997ae3d52fb3213a588
Serhii Yolkin <serhii.yolkin@gmail.com>
2022-07-13 20:37:27 +0200

    fix: fix BaseValidator rule check failing with inherited types

--- LostPolygon.AspNetCore.Utility/Validation/BaseValidator.cs
+++ LostPolygon.AspNetCore.Utility/Validation/BaseValidator.cs
@@ -5,7 +5,7 @@ using FluentValidation;
 using FluentValidation.Internal;
 using FluentValidation.Results;
 
-namespace LostPolygon.AspNetCore.Utility; 
+namespace LostPolygon.AspNetCore.Utility;
 
 public abstract class BaseValidator<T> : AbstractValidator<T> {
     protected BaseValidator() : this(true, true) {
@@ -28,9 +28,9 @@ public abstract class BaseValidator<T> : AbstractValidator<T> {
         List<PropertyRule> rules = this.Cast<PropertyRule>().ToList();
         PropertyInfo[] properties = typeof(T).GetProperties();
 
-        List<ValidationFailure> missingRulesFailures = new List<ValidationFailure>();
+        List<ValidationFailure> missingRulesFailures = new();
         foreach (PropertyInfo property in properties) {
-            bool ruleExists = rules.Any(rule => rule.Member == property);
+            bool ruleExists = rules.Any(rule => CheckMemberInfosEqual(rule.Member, property));
             if (!ruleExists) {
                 missingRulesFailures.Add(
                     new ValidationFailure(property.Name, $"Public property '{property.Name}' must have a validation rule.")
@@ -43,5 +43,11 @@ public abstract class BaseValidator<T> : AbstractValidator<T> {
 
         missingRulesFailures.Insert(0, new ValidationFailure("", $"All public properties must have a validation rule (type '{typeof(T).FullName}')."));
         throw new ValidationException(missingRulesFailures);
+    }
+
+    private static bool CheckMemberInfosEqual(MemberInfo x, MemberInfo b) {
+        return
+            x.DeclaringType == b.DeclaringType &&
+            x.MetadataToken == b.MetadataToken;
     }
 }
