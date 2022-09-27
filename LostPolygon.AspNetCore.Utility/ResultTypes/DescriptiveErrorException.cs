3b403f63aa4a379a002a6d14ca0761585cf19aac
Serhii Yolkin <serhii.yolkin@gmail.com>
2022-09-27 19:24:59 +0200

    feat: added DescriptiveErrorException

---
+++ LostPolygon.AspNetCore.Utility/ResultTypes/DescriptiveErrorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LostPolygon.AspNetCore.Utility.ResultTypes;
+
+public class DescriptiveErrorException : Exception {
+    public IDescriptiveError DescriptiveError { get; }
+
+    public DescriptiveErrorException(IDescriptiveError descriptiveError)
+        : base(descriptiveError.Message) {
+        DescriptiveError = descriptiveError;
+    }
+
+    public DescriptiveErrorException(IDescriptiveError descriptiveError, Exception? innerException)
+        : base(descriptiveError.Message, innerException) {
+        DescriptiveError = descriptiveError;
+    }
+}
