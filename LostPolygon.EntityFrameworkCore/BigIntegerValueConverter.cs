018b0ebc0921b66cd2d8df41a7a4392c08dda7f5
Serhii Yolkin <serhii.yolkin@gmail.com>
2023-10-27 16:52:35 +0200

    feat: added BigIntegerValueConverter

---
+++ LostPolygon.EntityFrameworkCore/BigIntegerValueConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Numerics;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace LostPolygon.EntityFrameworkCore;
+
+public class BigIntegerValueConverter : ValueConverter<BigInteger, string> {
+    public BigIntegerValueConverter() : base(Serialize, Deserialize, null)
+    {
+    }
+
+    private static readonly Expression<Func<string, BigInteger>> Deserialize = x => BigInteger.Parse(x);
+    private static readonly Expression<Func<BigInteger, string>> Serialize = x => x.ToString("R", System.Globalization.CultureInfo.InvariantCulture);
+}
