4b4caa062e5e1b289f82d24b9ddb856b3fd23866
Serhii Yolkin <serhii.yolkin@gmail.com>
2023-12-12 14:46:37 +0100

    fix: crash when configuration object has no properties

--- LostPolygon.AspNetCore.Utility/Configuration/ConfigurationExtraExtensions.cs
+++ LostPolygon.AspNetCore.Utility/Configuration/ConfigurationExtraExtensions.cs
@@ -2,12 +2,12 @@ using System;
 using FluentValidation;
 using Microsoft.Extensions.Configuration;
 
-namespace LostPolygon.AspNetCore.Utility; 
+namespace LostPolygon.AspNetCore.Utility;
 
 public static class ConfigurationExtensions {
     public static T GetAndValidate<T, TValidator>(this IConfigurationSection section, Action<T>? beforeValidate = null)
-        where TValidator : BaseOptionsValidator<T>, new() where T : class {
-        T config = section.Get<T>();
+        where TValidator : BaseOptionsValidator<T>, new() where T : class, new() {
+        T config = section.Get<T>() ?? new T();
         beforeValidate?.Invoke(config);
         TValidator validator = new TValidator();
         validator.ValidateAndThrow(config);
