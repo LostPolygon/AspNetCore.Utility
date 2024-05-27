b40df5292da61f01e9711bb478cddd0ca6120ccb
Serhii Yolkin <serhii.yolkin@gmail.com>
2024-05-27 18:43:38 +0200

    feat: added DisabledAttribute for disabling routes

---
+++ LostPolygon.AspNetCore.Utility/Utility/DisabledAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LostPolygon.AspNetCore.Utility;
+
+public class DisabledAttribute : ActionFilterAttribute, IAuthorizationFilter {
+    // ReSharper disable once UnusedParameter.Local
+    public DisabledAttribute(string reason) {
+    }
+
+    public override void OnActionExecuting(ActionExecutingContext context) {
+        context.Result = new NotFoundResult();
+    }
+
+    public void OnResourceExecuting(ResourceExecutingContext context) {
+        context.Result = new NotFoundResult();
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context) {
+        context.Result = new NotFoundResult();
+    }
+}
