cdcce3bffd56251df2834561afede162f510e5a4
Serhii Yolkin <serhii.yolkin@gmail.com>
2024-06-18 19:25:56 +0200

    feat: added PropertyBuilderExtensions

---
+++ LostPolygon.EntityFrameworkCore/PropertyBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LostPolygon.EntityFrameworkCore;
+
+public static class PropertyBuilderExtensions {
+    public static PropertyBuilder<TProperty> WithSimpleName<TProperty>(this PropertyBuilder<TProperty> propertyBuilder, string prefix = "") {
+        return propertyBuilder.HasColumnName(prefix + propertyBuilder.Metadata.GetDefaultColumnName());
+    }
+}
